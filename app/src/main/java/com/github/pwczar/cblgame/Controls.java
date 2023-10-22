6539433b13819ca8836ee32815b5e8c99b081695
fmarcelf <m.f.zelazny@student.tue.nl>
2023-10-23 01:32:32 +0200

    Implement Controls scene

---
+++ app/src/main/java/com/github/pwczar/cblgame/Controls.java
@@ -0,0 +1,39 @@
+package com.github.pwczar.cblgame;
+
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Controls implements Scene {
+    App app;
+    JButton back = new JButton("BACK");
+    JTextField howtoplay = new JTextField("HERE SHOULD BE CONTROLS EXPLAINED");
+
+    Controls(App app) {
+        this.app = app;
+    }
+
+    public void draw(Graphics g) {
+        g.clearRect(0, 0, app.frame.getWidth(), app.frame.getHeight());
+    }
+
+    public void run() {
+        app.frame.add(howtoplay, BorderLayout.CENTER);
+        app.frame.add(back, BorderLayout.SOUTH);
+        app.updateUI();
+
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app.frame.remove(howtoplay);
+                app.frame.remove(back);
+                Thread.currentThread().interrupt();
+                app.setScene(new Menu(app));
+                return;
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/github/pwczar/cblgame/Menu.java
+++ app/src/main/java/com/github/pwczar/cblgame/Menu.java
@@ -1,18 +1,22 @@
 package com.github.pwczar.cblgame;
 
 import java.awt.*;
-import java.awt.Graphics;
 import java.awt.event.*;
 import javax.swing.*;
+
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
 
 public class Menu implements Scene {
     App app;
 
     ImageIcon background = new ImageIcon("AABackground.jpg");
-    JLabel image = new JLabel("", background, JLabel.CENTER);
+    Image image = background.getImage().getScaledInstance(500, 800, Image.SCALE_SMOOTH);
+    JLabel imageLabel;
+    
     //TODO: Add background behind button
 
     JButton startButton = new JButton("START!");
+    JButton controlsButton = new JButton("Controls");
     
     Menu(App app) {
         this.app = app;
@@ -23,8 +27,13 @@ public class Menu implements Scene {
     }
 
     public void run() {
-        app.frame.add(image);
+        /* 
+        background.setImage(image);
+        imageLabel = new JLabel("", background, JLabel.CENTER);
+        app.frame.add(imageLabel, BorderLayout.CENTER);
+        */
         app.frame.add(startButton, BorderLayout.NORTH);
+        app.frame.add(controlsButton, BorderLayout.SOUTH);
         app.updateUI();
 
         startButton.addActionListener(new ActionListener() {
@@ -33,6 +42,17 @@ public class Menu implements Scene {
                 app.frame.remove(startButton);
                 Thread.currentThread().interrupt();
                 app.setScene(new Game(app));
+                return;
+            }
+        });
+
+        controlsButton.addActionListener(new ActionListener() {
+            @Override 
+            public void actionPerformed(ActionEvent e) {
+                app.frame.remove(startButton);
+                app.frame.remove(controlsButton);
+                Thread.currentThread().interrupt();
+                app.setScene(new Controls(app));
                 return;
             }
         });
