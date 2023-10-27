cdebb7feaafe8d4ce97570c824c589a747016157
pwczar <p.w.czarnecki@student.tue.nl>
2023-10-27 23:54:18 +0200

    Add BlockStateDestroyed

---
+++ app/src/main/java/com/github/pwczar/cblgame/BlockStateDestroyed.java
@@ -0,0 +1,10 @@
+package com.github.pwczar.cblgame;
+
+/**
+ * The state of a Block that has been destroyed.
+ */
+public class BlockStateDestroyed extends BlockStateRemoved {
+    BlockStateDestroyed(Block block) {
+        super(block);
+    }
+}
