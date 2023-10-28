2161d60aabe637f831498936fa0a31768a5e7266
pwczar <p.w.czarnecki@student.tue.nl>
2023-10-29 00:30:14 +0200

    Update Enemy.java

--- app/src/main/java/com/github/pwczar/cblgame/Enemy.java
+++ app/src/main/java/com/github/pwczar/cblgame/Enemy.java
@@ -1,14 +1,15 @@
 package com.github.pwczar.cblgame;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
+/**
+ * An Enemy which moves across the screen and attacks the player.
+ */
 public class Enemy extends Rectangle2D.Double implements Entity {
     static Random rand = new Random(System.currentTimeMillis());
-    static final int SIZE = 12;
 
     final Game game;
 
@@ -17,16 +18,25 @@ public class Enemy extends Rectangle2D.Double implements Entity {
     Image sprite;
     boolean flip = false;
 
+    /**
+     * Initialize an Enemy at x, y.
+     * @param game the game to work under
+     * @param x x coordinate
+     * @param y y coordinate
+     */
     Enemy(Game game, double x, double y) {
         this.game = game;
         vy = 20;
         this.x = x;
         this.y = y;
-        height = SIZE;
-        width = SIZE;
+        height = 12;
+        width = 12;
         sprite = game.loadSprite("witch.png");
     }
 
+    /**
+     * Draw an Enemy.
+     */
     public void draw(Graphics g) {
         if (!flip) {
             g.drawImage(sprite, (int) x, (int) y, null);
@@ -43,33 +53,37 @@ public class Enemy extends Rectangle2D.Double implements Entity {
         }
     }
 
+    /**
+     * Update an Enemy.
+     */
     public void update(double delta) {
+        // die from thrown Blocks
         for (Entity ent : game.getEntities()) {
-            if (ent instanceof Block) {
-                if (((Block) ent).intersects(this)) {
-                    Block block = (Block) ent;
-                    game.removeEntity(block);
-                    game.removeEntity(this);
-                }
+            if (ent instanceof Block
+                && ((Block) ent).intersects(this)) {
+                game.removeEntity((Block) ent);
+                game.removeEntity(this);
             }
         }
 
+        // destroy stacked Blocks
         for (Block block : game.grid.getBlocks()) {
             if (block.state instanceof BlockStateStacked
-                    && block.intersects(this)) {
+                && block.intersects(this)) {
                 block.state = new BlockStateDestroyed(block);
                 game.removeEntity(this);
             }
         }
-        if (intersects(game.floor)) {
+
+        if (intersects(game.floor) || intersects(game.player)) {
             game.removeEntity(this);
             game.player.hp -= 1;
             return;
-        } else {
-            y += delta * vy;
-            double yy = y / 20;
-            x = (game.getGameWidth() - SIZE + 1) * (Math.sin(yy) + 1) * 0.5;
-            flip = Math.cos(yy) < 0;
         }
+
+        y += delta * vy;
+        double yy = y / 20;
+        x = (game.getGameWidth() - width + 1) * (Math.sin(yy) + 1) * 0.5;
+        flip = Math.cos(yy) < 0;
     }
 }
