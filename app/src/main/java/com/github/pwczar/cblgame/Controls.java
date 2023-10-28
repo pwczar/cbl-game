d3e3297545e7d5b4ca188cf4b476137b58b02699
fmarcelf <m.f.zelazny@student.tue.nl>
2023-10-29 01:21:20 +0200

    Update Controls

--- app/src/main/java/com/github/pwczar/cblgame/Controls.java
+++ app/src/main/java/com/github/pwczar/cblgame/Controls.java
@@ -6,27 +6,43 @@ import javax.swing.*;
 
 public class Controls extends Scene {
     JButton back = new JButton("BACK");
-    JLabel rules = new JLabel("RULES :)");
+    JTextArea rules = new JTextArea("Ultimate goal of the game is to keep the witches"
+            + " from reaching the construction site."
+            + " In order to do that, Player has to use blocks to his advantage. Use 'E'"
+            + " to pick up/put down the block. While picked up, use 'F' to throw held block towards"
+            + " approaching witches. Stack of 3 same colored blocks give points,"
+            + " and if a block is of color other than grey, different power ups.");
 
     Controls(App app) {
         super(app);
     }
 
     public void draw(Graphics g) {
-        g.clearRect(0, 0, app.getWidth(), app.getHeight());
+        g.setColor(new Color(69, 36, 107));
+        g.fillRect(0, 0, app.getWidth(), app.getHeight());
     }
 
     public void run() {
-        back.setBackground(new Color(76, 9, 110));
+        back.setBackground(Color.BLACK);
         back.setForeground(new Color(243, 121, 7));
         back.setFont(new Font("", Font.BOLD, 25));
 
+        rules.setBackground(new Color(69, 36, 107));
+        rules.setForeground(new Color(243, 121, 7));
+        rules.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        rules.setLineWrap(true);
+        rules.setWrapStyleWord(true);
+        rules.setFont(new Font("", Font.BOLD, 15));
+        rules.setMaximumSize(new Dimension(500, 400));
+
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         back.setAlignmentX(Component.CENTER_ALIGNMENT);
+        rules.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         add(Box.createRigidArea(new Dimension(10, app.getHeight() / 4)));
         add(back);
         add(Box.createRigidArea(new Dimension(10, 10)));
+        rules.setEditable(false);
         add(rules);
 
         back.addActionListener(new ActionListener() {
