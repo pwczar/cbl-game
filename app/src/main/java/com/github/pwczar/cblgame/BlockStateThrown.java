cb99cddf72ac1f65e7237d3940fff9d47a315b81
pwczar <p.w.czarnecki@student.tue.nl>
2023-10-28 19:51:17 +0200

    Add block trail

--- app/src/main/java/com/github/pwczar/cblgame/BlockStateThrown.java
+++ app/src/main/java/com/github/pwczar/cblgame/BlockStateThrown.java
@@ -1,13 +1,35 @@
 package com.github.pwczar.cblgame;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
 /**
  * The state of a Block that has been thrown by the player.
  */
 public class BlockStateThrown extends BlockState {
-    double vy = -100;
+    double vy = -160;
+    Image trailImage;
 
+    /**
+     * Initialize BlockStateThrown and load trail image.
+     * @param block the block to depend on
+     */
     BlockStateThrown(Block block) {
         super(block);
+        trailImage = block.game.loadSprite("trail.png");
+    }
+
+    /**
+     * Draw a trail after the block.
+     */
+    public void draw(Graphics g) {
+        super.draw(g);
+        g.drawImage(
+            trailImage,
+            (int) block.x,
+            (int) block.y + Block.SIZE,
+            null
+        );
     }
 
     /**
--- app/src/main/java/com/github/pwczar/cblgame/Player.java
+++ app/src/main/java/com/github/pwczar/cblgame/Player.java
@@ -1,7 +1,6 @@
 package com.github.pwczar.cblgame;
 
 import java.awt.AlphaComposite;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
