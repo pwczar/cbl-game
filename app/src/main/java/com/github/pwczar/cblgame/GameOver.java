9d68c1371dee6c3c10e4c1e03004858b79014efe
fmarcelf <m.f.zelazny@student.tue.nl>
2023-10-29 18:16:00 +0100

    Update GameOver scene

--- app/src/main/java/com/github/pwczar/cblgame/GameOver.java
+++ app/src/main/java/com/github/pwczar/cblgame/GameOver.java
@@ -10,13 +10,14 @@ import javax.swing.*;
 public class GameOver extends Scene {
     JButton playAgainButton = new JButton("PLAY AGAIN!");
     JTextArea statsArea = new JTextArea();
-    JButton menuButton = new JButton("Go to the menu");
+    JButton menuButton = new JButton("Back to menu");
 
     Game game;
 
     /**
      * Initialize a game over screen.
-     * @param app the app to work under
+     * 
+     * @param app  the app to work under
      * @param game the game to show data from
      */
     GameOver(App app, Game game) {
@@ -27,24 +28,33 @@ public class GameOver extends Scene {
         playAgainButton.setForeground(new Color(243, 121, 7));
         playAgainButton.setFont(new Font("", Font.BOLD, 25));
 
+        menuButton.setBackground(Color.BLACK);
+        menuButton.setForeground(new Color(243, 121, 7));
+        menuButton.setFont(new Font("", Font.BOLD, 25));
+
         statsArea.setText(String.format(
-            "You have survived %.2fs and scored %d points",
-            game.gameTime, game.score
-        ));
+                "You have survived %.2fs and scored %d points",
+                game.gameTime, game.score));
         statsArea.setFont(new Font("", Font.BOLD, 20));
         statsArea.setForeground(Color.WHITE);
         statsArea.setBackground(new Color(0, 0, 0, 0));
-        statsArea.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        statsArea.setBorder(javax.swing.BorderFactory.createEmptyBorder(0,
+                (app.getWidth() - (int) (game.getGameWidth() * Game.SCALE)) / 2,
+                0,
+                (app.getWidth() - (int) (game.getGameWidth() * Game.SCALE)) / 2));
         statsArea.setLineWrap(true);
         statsArea.setWrapStyleWord(true);
         statsArea.setMaximumSize(getMaximumSize());
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         playAgainButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        menuButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         statsArea.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         add(Box.createRigidArea(new Dimension(10, app.getHeight() / 4)));
         add(playAgainButton);
+        add(Box.createRigidArea(new Dimension(10, 10)));
+        add(menuButton);
         add(Box.createRigidArea(new Dimension(10, 10)));
         add(statsArea);
     }
@@ -55,12 +65,15 @@ public class GameOver extends Scene {
     public void draw(Graphics g) {
         g.clearRect(0, 0, app.getWidth(), app.getHeight());
         g.drawImage(
-            game.lastFrame.getScaledInstance(app.getWidth(), app.getHeight(), Image.SCALE_SMOOTH),
-            0, 0, null
-        );
+                game.lastFrame.getScaledInstance(app.getWidth(), app.getHeight(), Image.SCALE_SMOOTH),
+                0, 0, null);
     }
 
     public void update(double delta) {
+        statsArea.setBorder(javax.swing.BorderFactory.createEmptyBorder(0,
+                (app.getWidth() - (int) (game.getGameWidth() * Game.SCALE)) / 2,
+                0,
+                (app.getWidth() - (int) (game.getGameWidth() * Game.SCALE)) / 2));
     }
 
     public void run() {
@@ -68,6 +81,13 @@ public class GameOver extends Scene {
             @Override
             public void actionPerformed(ActionEvent e) {
                 app.setScene(new Game(app));
+            }
+        });
+
+        menuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app.setScene(new Menu(app));
             }
         });
     }
